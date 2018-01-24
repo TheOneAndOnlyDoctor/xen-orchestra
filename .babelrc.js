40482b0880f3fbcf458413538280b554b2a15118
Julien Fontanet <julien.fontanet@isonoe.net>
2018-01-24 17:50:19 +0100

    WiP: feat(build): update to Babel 7

---
+++ .babelrc.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const __PROD__ = NODE_ENV === 'production'
+const __TEST__ = NODE_ENV === 'test'
+
+const pkg = require('./package')
+
+module.exports = {
+  comments: !__PROD__,
+  ignore: __TEST__ ? undefined : [/\.spec\.js$/],
+  plugins: [
+    '@babel/proposal-class-properties',
+    '@babel/proposal-decorators',
+    '@babel/proposal-export-default-from',
+    '@babel/proposal-function-bind',
+    '@babel/proposal-optional-chaining',
+    '@babel/proposal-pipeline-operator',
+    'dev',
+    'lodash',
+  ],
+  presets: [
+    [
+      '@babel/env',
+      {
+        debug: !__TEST__,
+        loose: true,
+        shippedProposals: true,
+        targets: __PROD__
+          ? { browsers: '>2%' }
+          : { node: 'current' },
+        useBuiltIns: '@babel/polyfill' in (pkg.dependencies || {}) && 'usage',
+      },
+    ],
+    '@babel/flow',
+    [
+      '@babel/react',
+      {
+        development: !__PROD__,
+      },
+    ],
+  ],
+}
